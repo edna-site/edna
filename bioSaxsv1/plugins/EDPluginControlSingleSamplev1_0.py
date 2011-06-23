79db575eebd54a119794b732e4b8b0a4ab64f601
kieffer <kieffer@4fc0fbde-e734-0410-b98b-b551705f41f8>
2011-06-23 15:53:30 +0000

    EDNA-124
    git-svn-id: https://forge.epn-campus.eu/svn/edna/trunk/edna@3485 4fc0fbde-e734-0410-b98b-b551705f41f8

--- bioSaxsv1/plugins/EDPluginControlSingleSamplev1_0.py
+++ bioSaxsv1/plugins/EDPluginControlSingleSamplev1_0.py
@@ -27,15 +27,17 @@ __author__ = "Jérôme Kieffer"
 __license__ = "GPLv3+"
 __copyright__ = "2011, ESRF Grenoble"
 
-
+import os, shutils
 from EDPluginControl import EDPluginControl
 from XSDataBioSaxsv1_0 import XSDataInputBioSaxsSingleSamplev1_0, XSDataResultBioSaxsSingleSamplev1_0
 
 class EDPluginControlSingleSamplev1_0(EDPluginControl):
     """
-    Plugin that does the processing a sample: (1 protein at 1 concentration)
-    
-    
+    Plugin that does the processing a sample: 
+    - 1 protein 
+    - 1 concentration
+    - 2+ serie of buffer files
+    - 1+ serie of sample files 
     """
 
 
@@ -44,24 +46,39 @@ class EDPluginControlSingleSamplev1_0(EDPluginControl):
         """
         EDPluginControl.__init__(self)
         self.setXSDataInputClass(None)
-        self.__strControlledPluginName = "EDPluginControlBioSaxsSmartMergev1_0"
-        self.__edPluginExecTemplate = None
+        self.__strControlledMerge = "EDPluginControlBioSaxsSmartMergev1_0"
+        self.__strControlledProcessOneFile = "EDPluginControlBioSaxsProcessOneFilev1_0"
+        self.__edPluginMerge = None
 
+        self.strDirectory1D = None
+        self.strDirectory2D = None
+        self.strDirectoryMisc = None
 
     def checkParameters(self):
         """
         Checks the mandatory parameters.
         """
         self.DEBUG("EDPluginControlSingleSamplev1_0.checkParameters")
-        self.checkMandatoryParameters(self.getDataInput(), "Data Input is None")
-
+        self.checkMandatoryParameters(self.dataInput, "Data Input is None")
+        self.checkMandatoryParameters(self.dataInput.directory1D, "No target Directory for 1D curves provided")
+        self.checkMandatoryParameters(self.dataInput.directory2D , "No target Directory for 2D curves provided")
+        self.checkMandatoryParameters(self.dataInput.directoryMisc , "No target Directory for Misc data provided")
+        self.checkMandatoryParameters(self.dataInput.backgroundSeries, "No target Directory for background data")
+        self.checkMandatoryParameters(self.dataInput.sampleSeries, "No target Directory for data ...")
 
     def preProcess(self, _edObject=None):
         EDPluginControl.preProcess(self)
         self.DEBUG("EDPluginControlSingleSamplev1_0.preProcess")
         # Load the execution plugin
-        self.__edPluginExecTemplate = self.loadPlugin(self.__strControlledPluginName)
+        self.__edPluginMerge = self.loadPlugin(self.__strControlledMerge)
 
+#
+#        self.strD    directory1D : XSDataFile
+#    directory2D : XSDataFile
+#    directoryMisc : XSDataFile
+#    backgroundSeries: XSDataFileSeries []
+#    sampleSeries: XSDataFileSeries []
+#
 
     def process(self, _edObject=None):
         EDPluginControl.process(self)
