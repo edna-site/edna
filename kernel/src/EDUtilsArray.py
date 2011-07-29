c822dee2fd3c6114d15f35b7e0a4e8cdb0829ec5
kieffer <kieffer@4fc0fbde-e734-0410-b98b-b551705f41f8>
2011-07-29 13:41:00 +0000

    Related to bug 784 ...
    git-svn-id: https://forge.epn-campus.eu/svn/edna/trunk/edna@3573 4fc0fbde-e734-0410-b98b-b551705f41f8

--- kernel/src/EDUtilsArray.py
+++ kernel/src/EDUtilsArray.py
@@ -25,26 +25,43 @@
 #    and the GNU Lesser General Public License  along with this program.  
 #    If not, see <http://www.gnu.org/licenses/>.
 #
+from __future__ import with_statement
 __authors__ = [ "Jérôme Kieffer" ]
 __contact__ = "jerome.kieffer@esrf.fr"
 __license__ = "LGPLv3+"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "20110721"
 
+import base64, hashlib, sys, struct, threading, os
 
-import base64, hashlib, sys, struct, threading
+from EDVerbose              import EDVerbose
+from EDObject               import EDObject
+from XSDataCommon           import XSDataArray, XSDataString, XSDataImageExt, XSDataFile
+from EDAssert               import EDAssert
+from EDShare                import EDShare
+from EDUtilsPlatform        import EDUtilsPlatform
+from EDFactoryPluginStatic  import EDFactoryPluginStatic
 
-from EDVerbose import EDVerbose
-from EDObject import EDObject
-from XSDataCommon import XSDataArray, XSDataString
-from EDAssert import EDAssert
+architecture = EDUtilsPlatform.architecture
+fabioPath = os.path.join(os.environ["EDNA_HOME"], "libraries", "FabIO-0.0.7", architecture)
+imagingPath = os.path.join(os.environ["EDNA_HOME"], "libraries", "20091115-PIL-1.1.7", architecture)
+numpyPath = os.path.join(os.environ["EDNA_HOME"], "libraries", "20090405-Numpy-1.3", architecture)
 
-try:
-    import numpy
+EDFactoryPluginStatic.preImport("Image", imagingPath)
+numpy = EDFactoryPluginStatic.preImport("numpy", numpyPath, _strForceVersion="1.2", _strMethodVersion="version.version")
+fabio = EDFactoryPluginStatic.preImport("fabio", fabioPath, _strForceVersion="0.0.7", _strMethodVersion="version")
+
+if (numpy is not None) and ("floating" in dir(numpy)):
     bHaveNumpy = True
     floatType = (float, numpy.floating)
-except ImportError:
+else:
     bHaveNumpy = False
     floatType = float
+
+if  fabio is not None:
+    bHaveFabio = True
+else:
+    bHaveFabio = False
 
 
 class EDUtilsArray(object):
@@ -75,10 +92,11 @@ class EDUtilsArray(object):
                  "float64": "d"}
     semArrayToXSData = threading.Semaphore()
     semXsDataToArray = threading.Semaphore()
+    semGetArray = threading.Semaphore()
 
 
-    @staticmethod
-    def arrayToXSData(_array, _bIncludeMd5sum=True, _bForceNoNumpy=False):
+    @classmethod
+    def arrayToXSData(cls, _array, _bIncludeMd5sum=True, _bForceNoNumpy=False):
         """
         convert a numpy array or a list of list into an XSDataArray object
         @param _array: numpy array or array-like list  
@@ -89,83 +107,83 @@ class EDUtilsArray(object):
         @type includeMd5sum: boolean
         @return: XSDataArray instance
         """
-        EDUtilsArray.semArrayToXSData.acquire()
-        xsdArray = XSDataArray()
-        stringArray = ""
-        shape = None
-        dtype = None
-        sizeDtype = None
-        if bHaveNumpy is True and _bForceNoNumpy is False:
-            EDVerbose.DEBUG("EDUtilsArray.arrayToXSData with numpy")
-            # Enforce little Endianness
-            if sys.byteorder == "big":
-                _array.byteswap(True)
-            stringArray = _array.tostring()
-            shape = _array.shape
-            dtype = str(_array.dtype)
-            sizeDtype = len(numpy.zeros(1, dtype=_array.dtype).tostring())
+        if True:
+#        with cls.semArrayToXSData:
+            xsdArray = XSDataArray()
+            stringArray = ""
+            shape = None
+            dtype = None
+            sizeDtype = None
+            if bHaveNumpy is True and _bForceNoNumpy is False:
+                EDVerbose.DEBUG("EDUtilsArray.arrayToXSData with numpy")
+                # Enforce little Endianness
+                if sys.byteorder == "big":
+                    _array.byteswap(True)
+                stringArray = _array.tostring()
+                shape = _array.shape
+                dtype = str(_array.dtype)
+                sizeDtype = len(numpy.zeros(1, dtype=_array.dtype).tostring())
 
-        else:
-            EDVerbose.DEBUG("EDUtilsArray.arrayToXSData without numpy")
-            sizeDtype = 8 # We enforce either double (float64) or int64 
-            shape = []
-            subarray = _array
-            while True:
-                try:
-                    l = len(subarray)
-                except TypeError:
-                    break
-                shape.append(l)
-                if l > 0:
-                    subarray = subarray[0]
-                else:
-                    break
-
-            if len(shape) == 1:
-                if isinstance(_array[0], floatType):
-                    dtype = "float64"
-                    stringArray = struct.pack("<" + "d" * shape[0], *_array)
-                else:
-                    dtype = "int64"
-                    stringArray = struct.pack("<" + "l" * shape[0], *_array)
-            elif len(shape) == 2:
-                if isinstance(_array[0][0], floatType):
-                    dtype = "float64"
-                    lineshape = "<" + "d" * shape[-1]
-                else:
-                    dtype = "int64"
-                    lineshape = "<" + "q" * shape[-1]
-                for subarray in _array:
-                    stringArray += struct.pack(lineshape, *subarray)
-            elif len(shape) == 3:
-                if isinstance(_array[0][0][0], floatType):
-                    dtype = "float64"
-                    lineshape = "<" + "d" * shape[-1]
-                else:
-                    dtype = "int64"
-                    lineshape = "<" + "q" * shape[-1]
-                for subarray in _array:
-                    for subsubarray in subarray:
-                        stringArray += struct.pack(lineshape, *subsubarray)
             else:
-                EDVerbose.WARNING("EDUtilsArray.arrayToXSDataArray: Array too large %s " % (shape))
-        xsdArray.setData(base64.b64encode(stringArray))
-        xsdArray.setCoding(XSDataString("base64"))
-        xsdArray.setShape(list(shape))
-        xsdArray.setDtype(dtype)
-        size = 1
-        for i in shape:
-            size *= i
-        xsdArray.setSize(size)
-        EDAssert.equal(size * sizeDtype, len(stringArray), "string representing the array has the right size")
-        if _bIncludeMd5sum is True:
-            xsdArray.setMd5sum(XSDataString(hashlib.md5(stringArray).hexdigest()))
-        EDUtilsArray.semArrayToXSData.release()
+                EDVerbose.DEBUG("EDUtilsArray.arrayToXSData without numpy")
+                sizeDtype = 8 # We enforce either double (float64) or int64 
+                shape = []
+                subarray = _array
+                while True:
+                    try:
+                        l = len(subarray)
+                    except TypeError:
+                        break
+                    shape.append(l)
+                    if l > 0:
+                        subarray = subarray[0]
+                    else:
+                        break
+
+                if len(shape) == 1:
+                    if isinstance(_array[0], floatType):
+                        dtype = "float64"
+                        stringArray = struct.pack("<" + "d" * shape[0], *_array)
+                    else:
+                        dtype = "int64"
+                        stringArray = struct.pack("<" + "l" * shape[0], *_array)
+                elif len(shape) == 2:
+                    if isinstance(_array[0][0], floatType):
+                        dtype = "float64"
+                        lineshape = "<" + "d" * shape[-1]
+                    else:
+                        dtype = "int64"
+                        lineshape = "<" + "q" * shape[-1]
+                    for subarray in _array:
+                        stringArray += struct.pack(lineshape, *subarray)
+                elif len(shape) == 3:
+                    if isinstance(_array[0][0][0], floatType):
+                        dtype = "float64"
+                        lineshape = "<" + "d" * shape[-1]
+                    else:
+                        dtype = "int64"
+                        lineshape = "<" + "q" * shape[-1]
+                    for subarray in _array:
+                        for subsubarray in subarray:
+                            stringArray += struct.pack(lineshape, *subsubarray)
+                else:
+                    EDVerbose.WARNING("EDUtilsArray.arrayToXSDataArray: Array too large %s " % (shape))
+            xsdArray.setData(base64.b64encode(stringArray))
+            xsdArray.setCoding(XSDataString("base64"))
+            xsdArray.setShape(list(shape))
+            xsdArray.setDtype(dtype)
+            size = 1
+            for i in shape:
+                size *= i
+            xsdArray.setSize(size)
+            EDAssert.equal(size * sizeDtype, len(stringArray), "string representing the array has the right size")
+            if _bIncludeMd5sum is True:
+                xsdArray.setMd5sum(XSDataString(hashlib.md5(stringArray).hexdigest()))
         return xsdArray
 
 
-    @staticmethod
-    def xsDataToArray(_xsdata, _bCheckMd5sum=True, _bForceNoNumpy=False):
+    @classmethod
+    def xsDataToArray(cls, _xsdata, _bCheckMd5sum=True, _bForceNoNumpy=False):
         """
         convert a XSDataArray into either a numpy array or a list of list 
         @param _xsdata: XSDataArray instance  
@@ -177,62 +195,104 @@ class EDUtilsArray(object):
         @type _bForceNoNumpy: boolean
         @return: numpy array or array-like list depending on what is available on the computer
         """
-        EDUtilsArray.semXsDataToArray.acquire()
-        shape = tuple(_xsdata.getShape())
-        encData = _xsdata.getData()
-        iSize = _xsdata.getSize()
-        dtype = str(_xsdata.getDtype())
+        if True:
+#        with cls.semXsDataToArray:
+            shape = tuple(_xsdata.getShape())
+            encData = _xsdata.getData()
+            iSize = _xsdata.getSize()
+            dtype = str(_xsdata.getDtype())
 
-        if _xsdata.getCoding() is not None:
-            strCoding = _xsdata.getCoding().getValue()
-            if strCoding == "base64":
-                decData = base64.b64decode(encData)
-            elif strCoding == "base32":
-                decData = base64.b32decode(encData)
-            elif strCoding == "base16":
-                decData = base64.b16decode(encData)
+            if _xsdata.getCoding() is not None:
+                strCoding = _xsdata.getCoding().getValue()
+                if strCoding == "base64":
+                    decData = base64.b64decode(encData)
+                elif strCoding == "base32":
+                    decData = base64.b32decode(encData)
+                elif strCoding == "base16":
+                    decData = base64.b16decode(encData)
+                else:
+                    EDVerbose.WARNING("Unable to recognize the encoding of the data !!! got %s, expected base64, base32 or base16, I assume it is base64 " % strCoding)
+                    decData = base64.b64decode(encData)
             else:
-                EDVerbose.WARNING("Unable to recognize the encoding of the data !!! got %s, expected base64, base32 or base16, I assume it is base64 " % strCoding)
+                EDVerbose.WARNING("No coding provided, I assume it is base64 ")
+                strCoding = "base64"
                 decData = base64.b64decode(encData)
-        else:
-            EDVerbose.WARNING("No coding provided, I assume it is base64 ")
-            strCoding = "base64"
-            decData = base64.b64decode(encData)
-        EDVerbose.DEBUG("Reading numpy array: len(EncData)= %s, len(decData)=%s, shape=%s, size=%s, dtype=%s, coding=%s" % (len(encData), len(decData), shape, _xsdata.getSize(), _xsdata.getDtype(), strCoding))
-        EDAssert.equal(len(decData), EDUtilsArray.dTypeSize[dtype] * iSize, "decoded data has the expected size")
+            EDVerbose.DEBUG("Reading numpy array: len(EncData)= %s, len(decData)=%s, shape=%s, size=%s, dtype=%s, coding=%s" % (len(encData), len(decData), shape, _xsdata.getSize(), _xsdata.getDtype(), strCoding))
+            EDAssert.equal(len(decData), cls.dTypeSize[dtype] * iSize, "decoded data has the expected size")
 
-        if (_xsdata.getMd5sum() is not None) and (_bCheckMd5sum is True):
-            EDAssert.equal(_xsdata.getMd5sum().getValue() , hashlib.md5(decData).hexdigest(), "md5sum is correct")
+            if (_xsdata.getMd5sum() is not None) and (_bCheckMd5sum is True):
+                EDAssert.equal(_xsdata.getMd5sum().getValue() , hashlib.md5(decData).hexdigest(), "md5sum is correct")
 
-        if bHaveNumpy is True and _bForceNoNumpy is False:
-            EDVerbose.DEBUG("EDUtilsArray.xsDataToArray with numpy")
-            try:
-                matIn = numpy.fromstring(decData, dtype=_xsdata.getDtype())
-            except:
-                matIn = numpy.fromstring(decData, dtype=numpy.dtype(str(_xsdata.getDtype())))
-            arrayOut = matIn.reshape(shape)
-            # Enforce little Endianness
-            if sys.byteorder == "big":
-                arrayOut.byteswap(True)
-
-        else:
-            EDVerbose.DEBUG("EDUtilsArray.xsDataToArray without numpy")
-            arrayOut = []
-            lineshape = "<" + EDUtilsArray.dTypeFormat[dtype] * shape[-1]
-            if len(shape) == 1:
-                arrayOut = list(struct.unpack(lineshape, decData))
-            elif len(shape) == 2:
-                linesize = shape[-1] * EDUtilsArray.dTypeSize[dtype]
-                for i in xrange(shape[0]):
-                    arrayOut.append(list(struct.unpack(lineshape, decData[i * linesize :(i + 1) * linesize ])))
-            elif len(shape) == 3:
-                linesize = shape[-1] * EDUtilsArray.dTypeSize[dtype]
-                imgsize = shape[-2] * linesize
-                for i in xrange(shape[0]):
-                    subarray = []
-                    for j in xrange(shape[1]):
-                        subarray.append(list(struct.unpack(lineshape, decData[imgsize * i + j * linesize :imgsize * i + (j + 1) * linesize ])))
-                    arrayOut.append(subarray)
-
-        EDUtilsArray.semXsDataToArray.release()
+            if bHaveNumpy is True and _bForceNoNumpy is False:
+                EDVerbose.DEBUG("EDUtilsArray.xsDataToArray with numpy")
+                try:
+                    matIn = numpy.fromstring(decData, dtype=_xsdata.getDtype())
+                except:
+                    matIn = numpy.fromstring(decData, dtype=numpy.dtype(str(_xsdata.getDtype())))
+                arrayOut = matIn.reshape(shape)
+                # Enforce little Endianness
+                if sys.byteorder == "big":
+                    arrayOut.byteswap(True)
+            else:
+                EDVerbose.DEBUG("EDUtilsArray.xsDataToArray without numpy")
+                arrayOut = []
+                lineshape = "<" + cls.dTypeFormat[dtype] * shape[-1]
+                if len(shape) == 1:
+                    arrayOut = list(struct.unpack(lineshape, decData))
+                elif len(shape) == 2:
+                    linesize = shape[-1] * cls.dTypeSize[dtype]
+                    for i in xrange(shape[0]):
+                        arrayOut.append(list(struct.unpack(lineshape, decData[i * linesize :(i + 1) * linesize ])))
+                elif len(shape) == 3:
+                    linesize = shape[-1] * cls.dTypeSize[dtype]
+                    imgsize = shape[-2] * linesize
+                    for i in xrange(shape[0]):
+                        subarray = []
+                        for j in xrange(shape[1]):
+                            subarray.append(list(struct.unpack(lineshape, decData[imgsize * i + j * linesize :imgsize * i + (j + 1) * linesize ])))
+                        arrayOut.append(subarray)
         return arrayOut
+
+
+    @classmethod
+    def getArray(cls, _inputObject):
+        """
+        Tries to retrieve a numpy array from an Image or from a shared array or an XSD object
+        
+        As this method returns a numpy array, this method works only on plateform where numpy is available.
+        
+        @param _inputObject: XSDataArray or XSDataImageExt or XSDataFile
+        @return: numpy ndarray
+        """
+        if True:
+#        with cls.semGetArray:
+            npaOutput = None
+            bError = False
+            if isinstance(_inputObject, XSDataArray):
+                npaOutput = cls.xsDataToArray(_inputObject)
+            elif isinstance(_inputObject, XSDataImageExt):
+                    if _inputObject.array is not None:
+                        npaOutput = cls.xsDataToArray(_inputObject.array)
+                    elif _inputObject.shared is not None:
+                        npaOutput = EDShare[_inputObject.shared.value]
+                    elif (_inputObject.path is not None) and os.path.isfile(_inputObject.path.value):
+                        if bHaveFabio is True:
+                            try:
+                                npaOutput = fabio.open(_inputObject.path.value).data
+                            except:
+                                bError = True
+                        else:
+                            bError = True
+            elif isinstance(_inputObject, XSDataFile) and \
+                (_inputObject.path is not None) and \
+                os.path.isfile(_inputObject.path.value):
+                        if bHaveFabio is True:
+                            try:
+                                npaOutput = fabio.open(_inputObject.path.value).data
+                            except:
+                                bError = True
+                        else:
+                            bError = True
+            if bError is True:
+                EDVerbose.ERROR("EDUtilsArray.getArray works better on platform with numpy & fabio ... No solution found for you, sorry.%s%s " % (os.linesep, _inputObject.marshal()))
+        return npaOutput
