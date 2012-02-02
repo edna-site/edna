65fd4150267e6c588a2f627d1d0a5432ed8299cd
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2012-02-02 11:02:38 +0100

    Correct minor typo

--- execPlugins/plugins/EDPluginGroupShift-v1.0/src/MeasureOffset.py
+++ execPlugins/plugins/EDPluginGroupShift-v1.0/src/MeasureOffset.py
@@ -68,11 +68,11 @@ def shiftFFT(inp, shift, method="fftw"):
     """
     d0, d1 = inp.shape
     v0, v1 = shift
-    f0 = np.fft.ifftshift(np.arange(-d0 // 2, d0 // 2))
-    f1 = np.fft.ifftshift(np.arange(-d1 // 2, d1 // 2))
+    f0 = numpy.fft.ifftshift(numpy.arange(-d0 // 2, d0 // 2))
+    f1 = numpy.fft.ifftshift(numpy.arange(-d1 // 2, d1 // 2))
     m1, m0 = numpy.meshgrid(f1, f0)
-    e0 = np.exp(-2j * pi * v0 * m0 / float(d0))
-    e1 = np.exp(-2j * pi * v1 * m1 / float(d1))
+    e0 = numpy.exp(-2j * numpy.pi * v0 * m0 / float(d0))
+    e1 = numpy.exp(-2j * numpy.pi * v1 * m1 / float(d1))
     e = e0 * e1
     if method.startswith("fftw") and (fftw3 is not None):
 
@@ -87,7 +87,7 @@ def shiftFFT(inp, shift, method="fftw"):
         ifft()
         out = input / input.size
     else:
-        out = np.fft.ifft2(np.fft.fft2(inp) * e)
+        out = numpy.fft.ifft2(numpy.fft.fft2(inp) * e)
     return abs(out)
 
 def maximum_position(img):
@@ -188,8 +188,12 @@ def measure_offset(img1, img2, method="fftw", withLog=False):
         return offset
 
 def merge3(a, b, c, ROI=None):
+    """
+    @param: a, b, c: 3 2D-datasets
+    @param ROI: tuple of slices, i.e. (slice(1,513),slice(700,700+512))
+    """
     from scipy import ndimage
-    out = np.zeros(a.shape, dtype="float32")
+    out = numpy.zeros(a.shape, dtype="float32")
     out += a
     if ROI is not None:
         ac = a[ROI]
@@ -200,8 +204,8 @@ def merge3(a, b, c, ROI=None):
         bc = b
         cc = c
     shab = measure_offset(ac, bc)
-    out += ndimage.shift(b, shab, order=1, cval=b.mean(dtype=float), output_type="float32")
+    out += ndimage.shift(b, shab, order=1, cval=b.mean(dtype=float))
     shac = measure_offset(ac, cc)
-    out += ndimage.shift(c, shac, order=1, cval=c.mean(dtype=float), output_type="float32")
-    print shab, shac
+    out += ndimage.shift(c, shac, order=1, cval=c.mean(dtype=float))
+    print(shab, shac)
     return out / 3.0
