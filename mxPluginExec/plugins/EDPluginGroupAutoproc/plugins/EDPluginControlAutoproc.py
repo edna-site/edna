0326929b0e177dbe9fa144dfbf1d0b83833785bb
Thomas Boeglin <thomas.boeglin@esrf.fr>
2012-05-11 10:47:29 +0200

    complete autoproc as currently implemented

--- mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
+++ mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
@@ -55,7 +55,7 @@ class EDPluginControlAutoproc( EDPluginControl ):
         """
         """
         EDPluginControl.__init__(self)
-        self.setXSDataInputClass(XSDataMinimalXdsIn)
+        self.setXSDataInputClass(XSDataAutoprocInput)
 
     def checkParameters(self):
         """
@@ -65,11 +65,25 @@ class EDPluginControlAutoproc( EDPluginControl ):
         self.checkMandatoryParameters(self.dataInput, "Data Input is None")
         self.checkMandatoryParameters(self.dataInput.input_file, "No XDS input file")
 
+        # at least check for the xds input file existence before
+        # trying to start anything even if the first xds run does it
+        # anyway
+        if not os.path.isfile(self.dataInput.input_file.path.value):
+            EDVerbose.ERROR('the specified input file does not exist')
+            self.setFailure()
+            return
+
     def preProcess(self, _edObject = None):
         EDPluginControl.preProcess(self)
         self.DEBUG("EDPluginControlAutoproc.preProcess")
 
+        data_in = self.dataInput
+        xds_in = XSDataMinimalXdsIn()
+        xds_in.input_file = data_in.input_file.path
+
         self.xds_first = self.loadPlugin("EDPluginControlRunXds")
+        self.xds_first.dataInput = xds_in
+
         self.generate = self.loadPlugin("EDPluginXDSGenerate")
 
         self.first_res_cutoff = self.loadPlugin("EDPluginResCutoff")
@@ -87,34 +101,40 @@ class EDPluginControlAutoproc( EDPluginControl ):
         self.DEBUG("EDPluginControlAutoproc.process")
 
         # first XDS plugin run with supplied XDS file
-        EDVerbose.INFO("First XDS run...")
+        EDVerbose.screen("First XDS run...")
         self.xds_first.executeSynchronous()
         if self.xds_first.isFailure():
             EDVerbose.ERROR('first XDS run failed')
-            self.setFailure(True)
+            self.setFailure()
             return
-        EDVerbose.INFO("First XDS run successfully completed")
+        EDVerbose.screen("First XDS run successfully completed")
 
         # apply the first res cutoff with the res extracted from the first XDS run
-        EDVerbose.INFO("First resolution cutoff")
+        EDVerbose.screen("First resolution cutoff")
         xdsresult = self.xds_first.dataOutput
         res_cutoff_in = XSDataResCutoff()
         res_cutoff_in.xds_res = xdsresult
+        res_cutoff_in.completeness_entries = xdsresult.completeness_entries
+
+        #XXX: remove from the data model as it is just pass-through?
+        res_cutoff_in.total_completeness = xdsresult.total_completeness
+
         self.first_res_cutoff.dataInput = res_cutoff_in
         self.first_res_cutoff.executeSynchronous()
         if self.first_res_cutoff.isFailure():
             EDVerbose.ERROR("res cutoff failed")
             self.setFailure()
             return
-        EDVerbose.INFO("First resolution cutoff successfully completed")
+        EDVerbose.screen("First resolution cutoff successfully completed")
         resolution = self.first_res_cutoff.dataOutput.res
 
         # for the generate w/ and w/out anom we have to specify where
         # the first XDS plugin run took place
-        xds_run_path = os.path.abspath(self.xds_first.getWorkingDirectory())
+        xds_run_directory = os.path.abspath(self.xds_first.dataOutput.xds_run_directory.value)
+        EDVerbose.screen("the xds run took place in {}".format(xds_run_directory))
         generate_input = XSDataXdsGenerateInput()
-        generate_input.resolution = res
-        generate_input.previous_run_dir = XSDataString(xds_run_path)
+        generate_input.resolution = resolution
+        generate_input.previous_run_dir = XSDataString(xds_run_directory)
         self.generate.dataInput = generate_input
 
         self.generate.executeSynchronous()
@@ -127,7 +147,7 @@ class EDPluginControlAutoproc( EDPluginControl ):
         # we can now use the xds output parser on the two correct.lp
         # files, w/ and w/out anom
         parse_anom_input = XSDataXdsOutputFile()
-        parse_anom_input.correct_lp = XSDataPath()
+        parse_anom_input.correct_lp = XSDataFile()
         parse_anom_input.correct_lp.path = self.generate.dataOutput.correct_lp_anom
 
         self.parse_xds_anom.dataInput = parse_anom_input
@@ -140,7 +160,7 @@ class EDPluginControlAutoproc( EDPluginControl ):
 
         # now the other one w/out anom
         parse_noanom_input = XSDataXdsOutputFile()
-        parse_noanom_input.correct_lp = XSDataPath()
+        parse_noanom_input.correct_lp = XSDataFile()
         parse_noanom_input.correct_lp.path = self.generate.dataOutput.correct_lp_no_anom
 
         self.parse_xds_noanom.dataInput = parse_noanom_input
@@ -164,8 +184,6 @@ class EDPluginControlAutoproc( EDPluginControl ):
         self.res_cutoff_anom.executeSynchronous()
         if self.res_cutoff_anom.isFailure():
             EDVerbose.ERROR('res cutoff for anom data failed')
-            self.setFailure()
-            return
 
         # same for non anom
         res_cutoff_noanom_in = XSDataResCutoff()
@@ -174,19 +192,18 @@ class EDPluginControlAutoproc( EDPluginControl ):
 
         self.res_cutoff_noanom.executeSynchronous()
         if self.res_cutoff_noanom.isFailure():
-            EDVerbose.ERROR('res cutoff for anom data failed')
-            self.setFailure()
-            return
+            EDVerbose.ERROR('res cutoff for non anom data failed')
 
         # now we just have to run XScale to generate w/ and w/out
         # anom, merged and unmerged
 
         # We use another control plugin for that to isolate the whole thing
 
+#        if not self.res_cutoff_anom.isFailure():
         xscale_anom_in = XSDataXscaleInput()
 
         input_file = XSDataXscaleInputFile()
-        input_file.path = self.generate.dataOuput.hkl_anom
+        input_file.path = self.generate.dataOutput.hkl_anom
         input_file.res = self.res_cutoff_anom.dataOutput.res
 
         xscale_anom_in.xds_files = [input_file]
@@ -199,10 +216,10 @@ class EDPluginControlAutoproc( EDPluginControl ):
 
         if self.xscale_anom.isFailure():
             EDVerbose.ERROR('xscale anom/merge generation failed')
-            self.setFailure()
-            return
 
         # same for non anom code path
+
+        #if not self.res_cutoff_noanom.isFailure():
         xscale_noanom_in = XSDataXscaleInput()
 
         input_file = XSDataXscaleInputFile()
@@ -219,9 +236,6 @@ class EDPluginControlAutoproc( EDPluginControl ):
 
         if self.xscale_noanom.isFailure():
             EDVerbose.ERROR('xscale anom/merge generation failed')
-            self.setFailure()
-            return
-
 
 
     def postProcess(self, _edObject = None):
