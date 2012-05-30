d6e42dd540a83f3181af8e775601786166b49d24
Thomas Boeglin <thomas.boeglin@esrf.fr>
2012-05-30 09:12:53 +0200

    implement the sequence of actions, not tested yet

--- mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoprocMaster.py
+++ mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoprocMaster.py
@@ -29,15 +29,23 @@ __copyright__ = "<copyright>"
 
 
 import os.path
+import os
+from tempfile import mkstemp
 
+from EDConfiguration import EDConfiguration
 from EDPluginControl import EDPluginControl
 from EDVerbose import EDVerbose
 
-from XSDataCommon import XSDataFile, XSDataBoolean, XSDataString
+from XSDataCommon import XSDataFile, XSDataBoolean, XSDataString, XSDataInteger
 
 from XSDataAutoproc import XSDataAutoprocInput
 from XSDataISPyBv1_4 import XSDataInputStoreAutoProc
 from XSDataISPyBv1_4 import XSDataResultStoreAutoProc
+
+from EDFactoryPlugin import edFactoryPlugin
+edFactoryPlugin.loadModule("XSDataWaitFilev1_0")
+
+from XSDataWaitFilev1_0 import XSDataInputWaitFile
 
 from xdscfgparser import parse_xds_file, dump_xds_file
 
@@ -52,6 +60,24 @@ class EDPluginControlAutoprocMaster(EDPluginControl):
         """
         EDPluginControl.__init__(self)
         self.setXSDataInputClass(XSDataAutoprocInput)
+
+    def configure(self):
+        conf = self.getConfiguration()
+        output_path = EDConfiguration.getStringParamValue(conf, 'outputPath')
+        if output_path is None:
+            EDVerbose.ERROR('no output dir for ISPyB data specified, exiting')
+            self.setFailure()
+            return
+        if not os.path.exists(output_path):
+            EDVerbose.ERROR('the ISPyB output dir does not exist, exiting')
+            self.setFailure()
+            return
+        if output_path[-1] != '/':
+            # mkstemp if picky about that
+            output_path += '/'
+        fd, path = mkstemp(prefix=output_path, suffix='.xml')
+        os.close(fd)
+        self.output_file = path
 
     def checkParameters(self):
         """
@@ -73,6 +99,18 @@ class EDPluginControlAutoprocMaster(EDPluginControl):
         # we need to "fix" the input file so the SPOT_RANGE gets set
         # to a sensible value
         cfg = parse_xds_file(input_file)
+        data_range = cfg.get("DATA_RANGE=", None)
+        if data_range is None:
+            # max's code exits shortly after if there's no data range
+            self.ERROR('DATA_RANGE not specified in the XDS.INP file, exiting')
+            self.setFailure()
+            return
+        image_start, image_end = data_range
+        template = cfg["NAMED_TEMPLATE_OF_DATA_FILES="]
+
+        #that's the image we'll wait for
+        self.first_image = _template_to_image(template, image_start)
+
         spot_range = _spot_range(cfg)
         if len(spot_range) != 0:
             cfg['SPOT_RANGE='] = spot_range
@@ -86,13 +124,68 @@ class EDPluginControlAutoprocMaster(EDPluginControl):
         self.DEBUG("EDPluginControlAutoprocMaster.preProcess")
 
         # instantiate the plugins
-        self.wait_intput_files = self.loadPlugin("EDPluginWaitFile")
+        self.wait_input_files = self.loadPlugin("EDPluginWaitFile")
+        self.run_autoproc = self.loadPlugin("EDPluginExecAutoprocOnOar")
         self.ispyb_upload = self.loadPlugin("EDPluginISPyBStoreAutoProcv1_4")
+
+        wait_file_in = XSDataWaitFilev1_0()
+        filepath = XSDataFile()
+        filepath.path = XSDataString(self.first_image)
+        wait_file_in.expectedFile = filepath
+        wait_file_in.timeOut = XSDataInteger(240) # max uses 250
+        self.wait_input_files.dataInput = wait_file_in
 
 
     def process(self, _edObject=None):
         EDPluginControl.process(self)
         self.DEBUG("EDPluginControlAutoprocMaster.process")
+
+        self.wait_input_files.executeSynchronous()
+        if self.wait_input_files.isFailure() or self.wait_input_files.dataOutput.timedOut.value:
+            EDVerbose.ERROR('waiting for the first file to appear failed/timed out')
+            self.setFailure()
+            return
+
+        # first file's here
+
+        # the autoproc proper takes a subclass of our data model, with
+        # only an added path to serialize its output to
+        autoproc_input = XSDataAutoprocInput()
+        autoproc_input.input_file = self.dataInput.input_file
+        autoproc_input.completeness_cutoff = self.dataInput.completeness_cutoff
+        autoproc_input.res_override = self.dataInput.res_override
+        autoproc_input.isig_cutoff = self.dataInput.isig_cutoff
+        autoproc_input.data_collection_id = self.dataInput.data_collection_id
+        output_file = XSDataFile()
+        output_file.path = XSDataString(self.output_path)
+        autoproc_input.output_file = output_file
+
+        self.run_autoproc.dataInput = autoproc_input
+        self.run_autoproc.executeSynchronous()
+
+        if self.run_autoproc.isFailure():
+            # TODO: maybe distinguish between timeout and "other"
+            # failures
+            EDVerbose.ERROR('autoproc completed UNsuccessfully on the cluster')
+            self.setFailure()
+            return
+
+        # try to parse the output from the plugin, which sould be in the right format already
+        try:
+            autoproc_output = XSDataInputStoreAutoProc.parseFile(self.output_path)
+        except Exception:
+            # maybe output more details?
+            # err = sys.exc_info()[1]
+            EDVerbose.ERROR('could not parse the output file {0}'.format(self.output_path))
+            self.setFailure()
+            return
+
+        self.ispyb_upload.dataInput = autoproc_output
+        self.ispyb_upload.executeSynchronous()
+        if self.ispyb_upload.isFailure():
+            EDVerbose.ERROR('upload to ispyb failed. data still in {0}'.format(self.output_path))
+            self.setFailure()
+            return
 
 
     def postProcess(self, _edObject=None):
@@ -114,9 +207,9 @@ def _spot_range(cfg):
     startimage = data_range[0]
 
     # reuse more or less the same variable names
-    eranges = [[90, 85, 80 ,75 ,70 ,65 ,60],
+    eranges = [[90, 85, 80, 75, 70, 65, 60],
                [45, 40, 35, 30, 25, 20, 15]]
-    res=list()
+    res = list()
     for range_ in eranges:
         for angle in range_:
             imnumber = startimage+(angle//osc_range)
