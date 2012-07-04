949270155125e37d0d5d972035395003d94ee579
Thomas Boeglin <thomas.boeglin@esrf.fr>
2012-07-04 16:44:53 +0200

    stupid mistake, 2nd

--- mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
+++ mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
@@ -147,11 +147,11 @@ class EDPluginControlAutoproc(EDPluginControl):
         template = conf['NAME_TEMPLATE_OF_DATA_FRAMES='][0]
         self.DEBUG('template for images is {0}'.format(template))
         # fix the path if it's not absolute
-        if not os.path.isabs(start_image):
-            self.DEBUG('file path {0} is not absolute'.format(start_image))
+        if not os.path.isabs(template):
+            self.DEBUG('file template {0} is not absolute'.format(template))
             base_dir = os.path.abspath(os.path.dirname(data_in.input_file.path.value))
-            start_image = os.path.normpath(os.path.join(base_dir, start_image))
-            self.DEBUG('file path fixed to {0}'.format(start_image))
+            template = os.path.normpath(os.path.join(base_dir, template))
+            self.DEBUG('file template fixed to {0}'.format(template))
 
         first_image = _template_to_image(template, start_image)
 
