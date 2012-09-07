990a2f3167a983ef89663081d44692e7dee4cbb5
Jerome Kieffer <jerome.kieffer@esrf.fr>
2012-09-07 14:37:38 +0200

    Update SaxsAnalysis (2s pipeline)

--- ednaSaxs/plugins/EDPluginControlSaxsAnalysisv1_0.py
+++ ednaSaxs/plugins/EDPluginControlSaxsAnalysisv1_0.py
@@ -34,7 +34,7 @@ import os
 from EDPluginControl import EDPluginControl
 from XSDataEdnaSaxs import XSDataInputSaxsAnalysis, XSDataResultSaxsAnalysis, \
                            XSDataInputAutoRg, XSDataInputDatGnom, XSDataInputDatPorod
-from XSDataCommon import XSDataString, XSDataLength, XSDataFile, XSDataInteger
+from XSDataCommon import XSDataString, XSDataLength, XSDataFile, XSDataInteger, XSDataStatus
 
 
 
@@ -60,14 +60,8 @@ class EDPluginControlSaxsAnalysisv1_0(EDPluginControl):
         self.scatterFile = None
         self.gnomFile = None
         self.autoRg = None
-        self.xGuinier = None
-        self.xGnom = None
-        self.xDmax = None
-        self.xTotal = None
+        self.gnom = None
         self.xVolume = None
-        self.XSDataRg = None
-        self.XSDataGnom = None
-        self.XSDataPorod = None
 
 
     def checkParameters(self):
@@ -87,31 +81,36 @@ class EDPluginControlSaxsAnalysisv1_0(EDPluginControl):
             self.gnomFile = self.dataInput.gnomFile.path.value
         else:
             self.gnomFile = os.path.join(self.getWorkingDirectory(), os.path.basename(self.scatterFile).split(".")[0] + ".out")
-        # Load the execution plugin
-        self.edPluginAutoRg = self.loadPlugin(self.cpAutoRg)
-        self.edPluginDatGnom = self.loadPlugin(self.cpDatGnom)
-        self.edPluginDatPorod = self.loadPlugin(self.cpDatPorod)
-        self.XSDataRg = XSDataInputAutoRg(inputCurve=[self.dataInput.scatterCurve])
-        self.XSDataGnom = XSDataInputDatGnom(inputCurve=self.dataInput.scatterCurve,
-                                             output=XSDataFile(XSDataString(self.gnomFile)))
-        self.XSDataPorod = XSDataInputDatPorod(gnomFile=XSDataFile(XSDataString(self.gnomFile)))
+        self.autoRg = self.dataInput.autoRg
+
 
     def process(self, _edObject=None):
         EDPluginControl.process(self)
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.process")
-        self.edPluginAutoRg.dataInput = self.XSDataRg
-        self.edPluginAutoRg.connectSUCCESS(self.doSuccessRg)
-        self.edPluginAutoRg.connectFAILURE(self.doFailureRg)
-        self.edPluginAutoRg.executeSynchronous()
+        if self.autoRg is None:
+            self.edPluginAutoRg = self.loadPlugin(self.cpAutoRg)
+            self.edPluginAutoRg.dataInput = XSDataInputAutoRg(inputCurve=[self.dataInput.scatterCurve])
+            self.edPluginAutoRg.connectSUCCESS(self.doSuccessRg)
+            self.edPluginAutoRg.connectFAILURE(self.doFailureRg)
+            self.edPluginAutoRg.executeSynchronous()
+
         if self.isFailure():
             return
-        self.edPluginDatGnom.dataInput = self.XSDataGnom
+
+        self.edPluginDatGnom = self.loadPlugin(self.cpDatGnom)
+        self.edPluginDatGnom.dataInput = XSDataInputDatGnom(inputCurve=self.dataInput.scatterCurve,
+                                             output=XSDataFile(XSDataString(self.gnomFile)),
+                                             rg=self.autoRg.rg,
+                                            skip=XSDataInteger(self.autoRg.firstPointUsed.value - 1))
         self.edPluginDatGnom.connectSUCCESS(self.doSuccessGnom)
         self.edPluginDatGnom.connectFAILURE(self.doFailureGnom)
         self.edPluginDatGnom.executeSynchronous()
+
         if self.isFailure():
             return
-        self.edPluginDatPorod.dataInput = self.XSDataPorod
+
+        self.edPluginDatPorod = self.loadPlugin(self.cpDatPorod)
+        self.edPluginDatPorod.dataInput = XSDataInputDatPorod(gnomFile=XSDataFile(XSDataString(self.gnomFile)))
         self.edPluginDatPorod.connectSUCCESS(self.doSuccessPorod)
         self.edPluginDatPorod.connectFAILURE(self.doFailurePorod)
         self.edPluginDatPorod.executeSynchronous()
@@ -122,13 +121,24 @@ class EDPluginControlSaxsAnalysisv1_0(EDPluginControl):
         EDPluginControl.postProcess(self)
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.postProcess")
         # Create some output data
-        xsDataResult = XSDataResultSaxsAnalysis(gnomFile=XSDataFile(XSDataString(self.gnomFile)),
-                                                autorg=self.autoRg,
-                                                rgGuinier=self.xGuinier,
-                                                rgGnom=self.xGnom,
-                                                dmax=self.xDmax,
-                                                total=self.xTotal,
-                                                volume=self.xVolume)
+        strLog = """Rg   =   %f +/- %f 
+I(0) =   %e +/- %e
+Points   %i to %i 
+Quality: %4.2f%%     Aggregated: %s
+Dmax    =    %12f       Total =   %12f     
+Guinier =    %12f       Gnom =    %12f     
+Volume  =    %12f""" % (self.autoRg.rg.value, self.autoRg.rgStdev.value,
+                        self.autoRg.i0.value, self.autoRg.i0Stdev.value,
+                        self.autoRg.firstPointUsed.value, self.autoRg.lastPointUsed.value,
+                        self.autoRg.quality.value * 100., self.autoRg.isagregated.value,
+                        self.gnom.dmax.value, self.gnom.total.value,
+                        self.gnom.rgGuinier.value, self.gnom.rgGnom.value,
+                        self.xVolume.value)
+
+        xsDataResult = XSDataResultSaxsAnalysis(autoRg=self.autoRg,
+                                                gnom=self.gnom,
+                                                volume=self.xVolume,
+                                                status=XSDataStatus(executiveSummary=XSDataString(strLog)))
         self.setDataOutput(xsDataResult)
 
 
@@ -136,8 +146,6 @@ class EDPluginControlSaxsAnalysisv1_0(EDPluginControl):
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.doSuccessRg")
         self.retrieveSuccessMessages(_edPlugin, "EDPluginControlSaxsAnalysisv1_0.doSuccessRg")
         self.autoRg = _edPlugin.dataOutput.autoRgOut[0]
-        self.XSDataGnom.rg = self.autoRg.rg
-        self.XSDataGnom.skip = XSDataInteger(self.autoRg.firstPointUsed.value - 1)
 
     def doFailureRg(self, _edPlugin=None):
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.doFailureRg")
@@ -147,10 +155,8 @@ class EDPluginControlSaxsAnalysisv1_0(EDPluginControl):
     def doSuccessGnom(self, _edPlugin=None):
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.doSuccessGnom")
         self.retrieveSuccessMessages(_edPlugin, "EDPluginControlSaxsAnalysisv1_0.doSuccessGnom")
-        self.xDmax = _edPlugin.dataOutput.dmax
-        self.xGnom = _edPlugin.dataOutput.rgGnom
-        self.xGuinier = _edPlugin.dataOutput.rgGuinier
-        self.xTotal = _edPlugin.dataOutput.total
+        self.gnom = _edPlugin.dataOutput.gnom
+        self.gnomFile = self.gnom.gnomFile.path.value
 
     def doFailureGnom(self, _edPlugin=None):
         self.DEBUG("EDPluginControlSaxsAnalysisv1_0.doFailureGnom")
