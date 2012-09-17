d03dde589eda425b5b02cfaf98cb2ede386dc032
Thomas Boeglin <thomas.boeglin@esrf.fr>
2012-09-17 14:38:25 +0200

    Remove conversions for unmerged datasets
    The current autoproc does only import merged datasets.

--- mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoprocImport.py
+++ mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoprocImport.py
@@ -41,7 +41,7 @@ from XSDataAutoproc import XSDataFileConversion
 
 OUTFILE_TEMPLATE = 'unmerged_{0}.mtz'
 
-class EDPluginAutoprocImport(EDPluginControl):
+class EDPluginControlAutoprocImport(EDPluginControl):
     def __init__(self):
         EDPluginControl.__init__(self)
         self.setXSDataInputClass(XSDataAutoprocImport)
@@ -52,38 +52,26 @@ class EDPluginAutoprocImport(EDPluginControl):
     def preProcess(self):
         EDPluginControl.preProcess(self)
         self.DEBUG('Import : preprocess')
-        self.anom_anom = self.loadPlugin('EDPluginControlFileConversion')
-        self.anom_noanom = self.loadPlugin('EDPluginControlFileConversion')
-        self.noanom_anom = self.loadPlugin('EDPluginControlFileConversion')
-        self.noanom_noanom = self.loadPlugin('EDPluginControlFileConversion')
+        self.anom = self.loadPlugin('EDPluginControlFileConversion')
+        self.noanom = self.loadPlugin('EDPluginControlFileConversion')
         tocopy = ['dataCollectionID', 'start_image', 'end_image', 'res', 'nres']
 
-        anom_anom_in = XSDataFileConversion()
-        anom_noanom_in = XSDataFileConversion()
-        noanom_anom_in = XSDataFileConversion()
-        noanom_noanom_in = XSDataFileConversion()
+        anom_in = XSDataFileConversion()
+        noanom_in = XSDataFileConversion()
 
         # copy the common attributes from our data model to the subplugins'
         for a in tocopy:
-            for dm in anom_anom_in, anom_noanom_in, noanom_anom_in, noanom_noanom_in:
+            for dm in anom_in, noanom_in
                 setattr(dm, a, getattr(self.dataInput, a))
 
         # now set the specific bits
-        anom_anom_in.anom = XSDataBoolean(True)
-        anom_anom_in.input_file = self.dataInput.input_anom_anom
-        anom_anom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('anom_anom'))
+        anom_in.anom = XSDataBoolean(True)
+        anom_in.input_file = self.dataInput.input_anom
+        anom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('anom'))
 
-        anom_noanom_in.anom = XSDataBoolean(False)
-        anom_noanom_in.input_file = self.dataInput.input_anom_noanom
-        anom_noanom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('anom_noanom'))
-
-        noanom_anom_in.anom = XSDataBoolean(True)
-        noanom_anom_in.input_file = self.dataInput.input_noanom_anom
-        noanom_anom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('noanom_anom'))
-
-        noanom_noanom_in.anom = XSDataBoolean(False)
-        noanom_noanom_in.input_file = self.dataInput.input_noanom_noanom
-        noanom_noanom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('noanom_noanom'))
+        noanom_in.anom = XSDataBoolean(False)
+        noanom_in.input_file = self.dataInput.input_noanom
+        noanom_in.output_file = os.path.join(self.outdir, OUTFILE_TEMPLATE.format('noanom'))
 
     def checkParameters(self):
         # NB. we'll only check for the output directory existence for
@@ -110,7 +98,6 @@ class EDPluginAutoprocImport(EDPluginControl):
         for p in self.getListOfLoadedPlugin():
             p.execute()
 
-        # wait for all the 4 plugins to finish
         self.synchronizePlugins()
 
     def postProcess(self):
@@ -120,10 +107,10 @@ class EDPluginAutoprocImport(EDPluginControl):
         res = XSDataAutoprocImportOut()
         status = XSDataStatus()
         res.status = status
-        all_good = all([not x.isFailure() for x in self.getListOfLoadedPlugin()])
+        all_good = not self.anom.isFailure() and not self.noanom.isFailure()
         status.isSuccess = XSDataBoolean(all_good)
         files = list()
-        for p in self.getListOfLoadedPlugin():
+        for p in [self.anom, self.noanom]:
             if not p.isFailure():
                 files.append(p.dataInput.output_file)
         res.files = files
