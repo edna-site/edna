066e9fbd35ba01eff3ef95ecbd6f3adb92f352f1
Thomas Boeglin <thomas.boeglin@esrf.fr>
2012-10-02 17:11:45 +0200

    pass the complete input file path to the file conversion plugin

--- mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
+++ mxPluginExec/plugins/EDPluginGroupAutoproc/plugins/EDPluginControlAutoproc.py
@@ -401,8 +401,9 @@ class EDPluginControlAutoproc(EDPluginControl):
         self.DEBUG('FINISHED xscale generation')
 
         import_in = XSDataAutoprocImport()
-        import_in.input_anom = self.xscale_generate.dataOutput.hkl_anom_merged
-        import_in.input_noanom = self.xscale_generate.dataOutput.hkl_noanom_merged
+        xscalepath = os.path.abspath(self.xscale_generate.getWorkingDirectory())
+        import_in.input_anom = os.path.join(xscalepath, self.xscale_generate.dataOutput.hkl_anom_merged)
+        import_in.input_noanom = os.path.join(xscalepath, self.xscale_generate.dataOutput.hkl_noanom_merged)
         import_in.dataCollectionId = self.dataInput.data_collection_id
         import_in.start_image = XSDataInteger(self.data_range[0])
         import_in.end_image = XSDataInteger(self.data_range[1])
